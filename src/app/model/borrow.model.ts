819919964c3cf139a7c6401032baf98692c5d837
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-06-20 05:32:07 +0600

    need to maek availavle false using static method

--- src/app/model/borrow.model.ts
+++ src/app/model/borrow.model.ts
@@ -1,6 +1,7 @@
 import { model, Schema, Types } from "mongoose";
 import { IBorrow } from "../interface/borrow.interface";
 import { number } from "zod";
+import { books } from "./books.model";
 
 const schemaBorrow = new Schema<IBorrow>(
   {
@@ -28,5 +29,31 @@ const schemaBorrow = new Schema<IBorrow>(
     versionKey: false,
   }
 );
+
+// Verify the book has enough available copies.
+// Deduct the requested quantity from the book’s copies.
+// If copies become 0, update available to false (implement this using a static method or instance method).
+// Save the borrow record with all relevant details.
+
+schemaBorrow.pre("save", async function (next) {
+  try {
+    const borrow = this as IBorrow;
+    const book = await books.findById(borrow.book);
+    if (!book) {
+      throw new Error("book not found");
+    }
+    if (book.copies < borrow.quantity) {
+      throw new Error(`not enough copies available! Copies : ${book.copies}`);
+    }
+    book.copies -= borrow.quantity;
+    if (book.copies === 0) {
+      book.available = false;
+    }
+    await book.save();
+    next();
+  } catch (error) {
+    next(error as Error);
+  }
+});
 
 export const borrow = model<IBorrow>("borrows", schemaBorrow);
