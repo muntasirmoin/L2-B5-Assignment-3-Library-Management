87d1e620fa77fbfbe122b0fbb37f69b5d57c39a6
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2025-06-20 13:01:48 +0600

    aggreation added

--- src/app/controller/borrow.controller.ts
+++ src/app/controller/borrow.controller.ts
@@ -3,6 +3,8 @@ import { borrow } from "../model/borrow.model";
 
 export const borrowRoute = Router();
 
+// 6. Borrow a Book
+
 borrowRoute.post("/", async (req: Request, res: Response, next) => {
   try {
     const borrowBody = req.body;
@@ -11,6 +13,28 @@ borrowRoute.post("/", async (req: Request, res: Response, next) => {
     res.status(201).json({
       success: true,
       message: "Book borrowed successfully",
+      data: data,
+    });
+  } catch (error) {
+    next(error);
+  }
+});
+
+// 7. Borrowed Books Summary (Using Aggregation)
+borrowRoute.get("/", async (req: Request, res: Response, next) => {
+  try {
+    const data = await borrow.aggregate([
+      {
+        $group: {
+          _id: "$book",
+          totalQuantity: { $sum: "$quantity" },
+        },
+      },
+    ]);
+
+    res.status(201).json({
+      success: true,
+      message: "Borrowed books summary retrieved successfully",
       data: data,
     });
   } catch (error) {
